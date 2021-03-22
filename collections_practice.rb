9bbaac1d3c8714149efdb1ac8cf6fa2582aacd73
Kristyn Little <kristynlittle@frontier.com>
2021-03-22 20:53:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort{|a,b|a <=> b}
+end
 
+def sort_array_desc(array)
+  array.sort {|x,y|y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|left,right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+  array[0], array[1] = array[1], array[0]
+end
